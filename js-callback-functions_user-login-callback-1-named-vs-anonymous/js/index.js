96ee4157e9ce2e83c54074cec945334ca99d614f
Yan Chen <ychen90@outlook.com>
2023-03-06 10:48:53 +0100

    add name/anonymous callback challenge

---
+++ js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -0,0 +1,21 @@
+console.clear();
+
+function handleUserLogin(onSuccess) {
+  // You might wonder why this function is called `onSuccess`. This will become clear by the fourth challenge! 🤞
+  onSuccess();
+}
+
+// Call handleUserLogin below!
+/*
+function showWelcomeMessage() {
+  console.log("Welcome! You are logged in now.");
+}
+
+handleUserLogin(showWelcomeMessage);
+*/
+
+/* question 2*/
+
+handleUserLogin(()=>{
+  console.log("Welcome! You are logged in now.")
+})
