bdb27bd9f179cb5298457690a14c7dd41b2f3f19
Yan Chen <ychen90@outlook.com>
2023-03-13 23:53:41 +0100

    add people challenge with tests

---
+++ js-unit-testing_people/index.js
@@ -0,0 +1,41 @@
+export function getFirstNames(people) {
+  return people.map((person) => person.firstName);
+}
+
+export function getFullNames(people) {
+  return people.map((person)=>{
+    return person.firstName + " " + person.lastName
+  })
+}
+
+export function getNameAndAge(people) {
+  return people.map((person) =>
+     `${person.lastName} (${person.age})`
+    );
+
+}
+
+export function getPeopleByAge(people, age) {
+  return people.filter((person) => person.age == age);
+}
+
+export function getPeopleNamesOlderThan(people, age) {
+  return getFullNames((people.filter((person) => person.age > age)));
+
+}
+
+export function getPeopleByLastName(people, lastName) {
+  return people.filter((person)=> person.lastName == lastName);
+}
+
+export function findPersonById(people, id) {
+  return people.filter((person) => person.id == id)
+}
+
+export function isAnyoneOlderThan(people, age) {
+  return people.some((person) => person.age > age )
+}
+
+export function getFullNamesSortedByAge(people) {
+  return getFullNames(people.sort((a, b) => a.age - b.age));
+}
