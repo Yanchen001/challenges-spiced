27e121ec142b8c1c539493a2723d4bdc9b3b0d6c
Yan Chen <ychen90@outlook.com>
2023-03-15 14:39:45 +0100

    Add react-basics-hello-world-article

---
+++ react-basics_hello-world-article/react-basics_hello-world-article/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <h1>Replace me with your Component!</h1>;
+}
+
+export function HelloWorldArticle() {
+  return <article><h1><p>HelloWorldArticle</p></h1></article>
+}
